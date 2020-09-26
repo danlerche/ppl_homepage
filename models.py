f94ddb6a238e63597fb54512d1a58ad25f6d7d49
dan <dan.lerch@gmail.com>
2020-09-26 13:29:27 -0700

    added option for external urls to the feature boxes.

--- models.py
+++ models.py
@@ -16,23 +16,35 @@ class HomePage(Page):
     background_image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     searchBoxTextArea = RichTextField(blank=True, verbose_name="Search Box Text Area",)
 
-    feature_one_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+',)
-    feature_one_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Link to a document instead of a page",)
+    program_slider_title = models.CharField(blank=True, max_length=250)
+
+    upcoming_events_title = models.CharField(blank=True, max_length=250)
+
+    feature_one_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Links to a internal page. Links to external pages or documents will be ignored",)
+    feature_one_ext_link = models.URLField(max_length=200, null=True, blank=True, verbose_name="Links to an external website instead of an internal cms page. This will only work if no internal page is selected.")
+    feature_one_ext_link_name = models.CharField(blank=True, max_length=250, verbose_name="Name of the external link")
+    feature_one_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Link to a document instead of a page or external url. This will only work if no internal page, or external link is selected",)
     feature_one_image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     feature_one_text = models.CharField(blank=True, max_length=250)
-    
-    feature_two_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+',)
-    feature_two_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Link to a document instead of a page",)
+
+    feature_two_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Links to a internal page. Links to external pages or documents will be ignored",)
+    feature_two_ext_link = models.URLField(max_length=200, null=True, blank=True, verbose_name="Links to an external website instead of an internal cms page. This will only work if no internal page is selected.")
+    feature_two_ext_link_name = models.CharField(blank=True, max_length=250, verbose_name="Name of the external link")
+    feature_two_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Link to a document instead of a page or external url. This will only work if no internal page, or external link is selected",)
     feature_two_image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     feature_two_text = models.CharField(blank=True, max_length=250)
 
-    feature_three_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+',)
-    feature_three_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+',verbose_name="Link to a document instead of a page",)
+    feature_three_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Links to a internal page. Links to external pages or documents will be ignored",)
+    feature_three_ext_link = models.URLField(max_length=200, null=True, blank=True, verbose_name="Links to an external website instead of an internal cms page. This will only work if no internal page is selected.")
+    feature_three_ext_link_name = models.CharField(blank=True, max_length=250, verbose_name="Name of the external link")
+    feature_three_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Link to a document instead of a page or external url. This will only work if no internal page, or external link is selected",)
     feature_three_image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     feature_three_text = models.CharField(blank=True, max_length=250)
 
-    feature_four_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+',)
-    feature_four_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+',verbose_name="Link to a document instead of a page",)
+    feature_four_page = models.ForeignKey('wagtailcore.Page',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Links to a internal page. Links to external pages or documents will be ignored",)
+    feature_four_ext_link = models.URLField(max_length=200, null=True, blank=True, verbose_name="Links to an external website instead of an internal cms page. This will only work if no internal page is selected.")
+    feature_four_ext_link_name = models.CharField(blank=True, max_length=250, verbose_name="Name of the external link")
+    feature_four_doc = models.ForeignKey('wagtaildocs.Document',null=True,blank=True,on_delete=models.SET_NULL,related_name='+', verbose_name="Link to a document instead of a page or external url. This will only work if no internal page, or external link is selected",)
     feature_four_image = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     feature_four_text = models.CharField(blank=True, max_length=250)
 
@@ -40,11 +52,10 @@ class HomePage(Page):
     newsletter_image  = models.ForeignKey('wagtailimages.Image', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     newsletter_text = models.CharField(blank=True, max_length=250)
 
-    content_panels = [
+    content_panels = Page.content_panels + [
 
         MultiFieldPanel(
             [
-
                 ImageChooserPanel('background_image', classname="full"),
                 FieldPanel('searchBoxTextArea', classname="full"),
             ],
@@ -53,11 +64,22 @@ class HomePage(Page):
             classname = "collapsible"
             ),
 
+         MultiFieldPanel(
+            [
+                FieldPanel('upcoming_events_title', classname="full"),
+            ],
+
+            heading = "Upcoming Events Title",
+            classname = "collapsible collapsed"
+            ),
+
         MultiFieldPanel(
             [
 
                 ImageChooserPanel('feature_one_image', classname="full"),
                 PageChooserPanel('feature_one_page'),
+                FieldPanel('feature_one_ext_link'),
+                FieldPanel('feature_one_ext_link_name'),
                 DocumentChooserPanel('feature_one_doc', classname="full"),
                 FieldPanel('feature_one_text', classname="full"),
             ],
@@ -71,6 +93,8 @@ class HomePage(Page):
 
                 ImageChooserPanel('feature_two_image', classname="full"),
                 PageChooserPanel('feature_two_page'),
+                FieldPanel('feature_two_ext_link'),
+                FieldPanel('feature_two_ext_link_name'),
                 DocumentChooserPanel('feature_two_doc'),
                 FieldPanel('feature_two_text', classname="full"),
             ],
@@ -95,6 +119,8 @@ class HomePage(Page):
 
                 ImageChooserPanel('feature_three_image', classname="full"),
                 PageChooserPanel('feature_three_page'),
+                FieldPanel('feature_three_ext_link'),
+                FieldPanel('feature_three_ext_link_name'),
                 DocumentChooserPanel('feature_three_doc'),
                 FieldPanel('feature_three_text', classname="full"),
             ],
@@ -108,6 +134,8 @@ class HomePage(Page):
 
                 ImageChooserPanel('feature_four_image', classname="full"),
                 PageChooserPanel('feature_four_page'),
+                FieldPanel('feature_four_ext_link'),
+                FieldPanel('feature_four_ext_link_name'),
                 DocumentChooserPanel('feature_four_doc'),
                 FieldPanel('feature_four_text', classname="full"),
             ],
@@ -128,8 +156,8 @@ class HomePage(Page):
 
           MultiFieldPanel(
             [
+                FieldPanel('program_slider_title', classname="Full"),
                 InlinePanel('program_slider', label="Program Slider"),
-
             ],
 
             heading = "Featured Programs ",
@@ -150,7 +178,7 @@ class StaffRecommends(Orderable):
     #('L', 'listen'),
     #)
     #read_watch_listen = models.CharField(max_length=1, choices=rwl_choices)
-    panels = [ImageChooserPanel('book_cover'),FieldPanel('catalogue_link'),#FieldPanel('read_watch_listen'), 
+    panels = [ImageChooserPanel('book_cover'),FieldPanel('catalogue_link'),#FieldPanel('read_watch_listen'),
     ]
 
 class ProgramSlider(Orderable):
